64aefa3a45f8a02d5b16e28a025de994447b6ecc
bulelaniskhikhi <bulelaniskhikhi@gmail.com>
2022-08-02 15:04:48 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav> -->
+
+  <nav class="navbar">
+  
+  <div class="container" >
+    <a class="navbar-brand" href="#">
+      <img src="https://i.postimg.cc/mrS7L64N/pick-n-pay-logo-vector-removebg-preview.png" style="height: 6rem" class="d-inline-block align-text-top">
+    </a>
+    <span class="navbar-text">
+        LOGIN
+    </span>
+
+  </div>
+
+</nav>
+
+  
+  <router-view/>
+  
+</template>
+
+<style>
+
+</style>
